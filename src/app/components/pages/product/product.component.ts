73f24f6c8f9e606cb29d07813c3033dfb460e169
root <root@buckapi>
2022-11-16 07:41:22 -0400

    product controller

--- src/app/components/pages/product/product.component.ts
+++ src/app/components/pages/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { SwiperOptions } from 'swiper';
 import {Butler} from '@app/services/butler.service';
 @Component({
   selector: 'app-product',
@@ -8,6 +8,14 @@ import {Butler} from '@app/services/butler.service';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+    config: SwiperOptions = {
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    spaceBetween: 30
+  };
 public editing:boolean=false
   constructor(
 public _butler:Butler,
--- src/app/components/pages/product/product.module.ts
+++ src/app/components/pages/product/product.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
-
+import { NgxUsefulSwiperModule } from 'ngx-useful-swiper';
 
 @NgModule({
   declarations: [
     ProductComponent
   ],
   imports: [
+  NgxUsefulSwiperModule,
     CommonModule,
     ProductRoutingModule
   ]
